9e5ce6b00f42d06eded826dbf894efc54e1b9e17
haikallf <haikallzrd@gmail.com>
2022-02-08 17:10:11 +0700

    Start tests for GuessedWords

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import GuessedWords from "./GuessedWords";
+import checkPropTypes from "check-prop-types";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefined();
+};
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test("does not throw warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+describe("if there are no words guessed", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe("if there are words guessed", () => {});
