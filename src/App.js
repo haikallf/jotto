a0cc963c14073aa284d204ce567890af172762f5
haikallf <haikallzrd@gmail.com>
2022-02-10 23:22:14 +0700

    add functional tests for guess word

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
+import Input from "./Input";
 
 function App() {
+  const success = false;
+  const secretWord = "party";
+  const guessedWords = [];
+
   return (
-    <div className="container">
+    <div data-test="component-app" className="container">
       <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords
-        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-      />
+      <Congrats success={success} />
+      <Input success={success} secretWord={secretWord} />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import { findByTestAttr } from "./functions";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import App from "./App";
 
-test("renders learn react link", () => {});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = () => {
+  return shallow(<App />);
+};
+
+test("renders without error", () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent).toHaveLength(1);
+});
