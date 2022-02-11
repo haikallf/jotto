ddabfc375e351cdf135d3e8ef909d7c5d665acf1
haikallf <haikallzrd@gmail.com>
2022-02-12 00:18:13 +0700

    add thunk

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,14 @@
 import checkPropTypes from "check-prop-types";
-import propTypes from "prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import { middlewares } from "./configureStore";
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middlewares)
+  );
 };
 
 export const findByTestAttr = (wrapper, val) => {
