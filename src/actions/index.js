e8097a5acf5b3e45835954db16c05fdd66401a64
haikallf <haikallzrd@gmail.com>
2022-02-12 00:28:41 +0700

    set up guessedWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,16 @@ import axios from "axios";
 
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
+  GUESS_WORD: "GUESS_WORD",
 };
 
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
-}
+// export function correctGuess() {
+//   return { type: actionTypes.CORRECT_GUESS };
+// }
+
+export const guessWord = () => {
+  return function (dispatch, getState) {};
+};
 
 export const getSecretWord = () => {
   return axios.get("http://localhost:3030").then((response) => response.data);
---
+++ src/integration.test.js
@@ -0,0 +1,16 @@
+import { storeFactory } from "./functions";
+import { guessedWord } from "./actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    test("updates state correctly for unsuccessful guess", () => {});
+
+    test("updates state correctly for successful guess", () => {});
+  });
+
+  describe("some guessed words", () => {
+    test("updates state correctly for unsuccessful guess", () => {});
+
+    test("updates state correctly for successful guess", () => {});
+  });
+});
