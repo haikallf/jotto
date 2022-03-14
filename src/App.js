1928d1dc0f5326958cb611e3b54c7c79cbcd07be
haikallf <haikallzrd@gmail.com>
2022-03-14 09:24:38 +0700

    set resetMocks to false, change mock return value

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
 import Input from "./Input";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getSecretWord } from "./actions";
 
 function App() {
   const success = useSelector((state) => state.success);
   const guessedWords = useSelector((state) => state.guessedWords);
-  const secretWord = "party";
+  const secretWord = useSelector(state => state.secretWord);
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    getSecretWord();
+    dispatch(getSecretWord());
   }, []);
 
   return (
